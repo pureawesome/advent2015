b6ec6a7ff01ce00c6c9ca4742fc1ee3db9e2ed38
Bartok <bartok@Bartoks-MacBook-Pro.local>
2015-12-09 20:44:28 -0800

    Days 1-4

---
+++ four.rb
@@ -0,0 +1,41 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require 'digest'
+
+class Four
+  def getHash(str)
+    count = 0
+    md5 = Digest::MD5.new
+    md5.hexdigest
+
+    while (count)
+      md5.reset
+      md5.update "#{str}#{count}"
+      if md5.to_s.slice(0,5) == "00000"
+        break
+      end
+      count += 1
+    end
+    count
+  end
+end
+
+class TestFour < Minitest::Test
+  def setup
+    @four = Four.new
+  end
+
+  def test_one
+    # skip
+    assert_equal 609043, @four.getHash("abcdef")
+  end
+
+  def test_two
+    # skip
+    assert_equal 1048970, @four.getHash("pqrstuv")
+  end
+end
+
+@run = Four.new
+p @run.getHash("ckczppom")
