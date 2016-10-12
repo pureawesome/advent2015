cc65d2cf13d2473a6bc03813baa6549b43306322
Gennaro Avolio-Toly <gennaro.avolio-toly@smashingideas.com>
2016-10-12 14:15:41 -0700

    fifteen part 2

--- fifteen.rb
+++ fifteen.rb
@@ -12,15 +12,23 @@ def get_total (ingredients)
   totals = []
   splits = get_splits(100, ingredients.count)
   splits.each do |split|
-    total = { capacity: 0, durability: 0, flavor: 0, texture: 0 }
+    total = { capacity: 0, durability: 0, flavor: 0, texture: 0, calories: 0 }
     ingredients.each.with_index do |ingred, index|
       ingred.each do |key, value|
-        total[key] += value * split[index] unless key == 'calories'.to_sym
+        # part 1
+        # total[key] += value * split[index] unless key == 'calories'.to_sym
+        # part 2
+        total[key] += value * split[index]
       end
     end
-
-    split_total = total.values.map { |num| num < 0 ? 0 : num }.inject(:*)
-    totals.push(split_total)
+    # part 2
+    if total[:calories] == 500
+      total[:calories] = 1
+      split_total = total.values.map { |num| num < 0 ? 0 : num }.inject(:*)
+      totals.push(split_total)
+    else
+      next
+    end
   end
   totals.max
 end
@@ -29,7 +37,8 @@ ingredients_test = [
   { capacity: -1, durability: -2, flavor: 6, texture: 3, calories: 8 },
   { capacity: 2, durability: 3, flavor: -2, texture: -1, calories: 3 }
 ]
-p get_total(ingredients_test)
+
+# p get_total(ingredients_test)
 
 ingredient_list = File.readlines('fifteen.txt').map do |line|
   arr = line.split(/[:|,|\n]/).map(&:strip)
