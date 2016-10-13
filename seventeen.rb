b1e0ac04adb0df4e445b6310660dfe00d61a529d
Gennaro Avolio-Toly <gennaro.avolio-toly@smashingideas.com>
2016-10-12 17:38:14 -0700

    day seventeen part 2

--- seventeen.rb
+++ seventeen.rb
@@ -1,11 +1,26 @@
 containers = File.readlines('seventeen.txt').map(&:to_i)
 
 combos_container = []
+lengths_container = []
 
 containers.count.times do |index|
   combos = containers
             .combination(index).to_a
             .delete_if { |array| array.inject(:+) != 150 }
+
+  # part 2
+  lengths = combos.map do |line|
+    if line.count == 1
+      line.each {|combo| p combo }
+    else
+      line.count
+    end
+  end
+
+  lengths_container.push(lengths)
   combos_container.push(combos)
 end
+# part 1
 p combos_container.flatten(1).count
+# part 2
+p lengths_container.reject(&:empty?)[0].count
