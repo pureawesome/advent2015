00f653a0b90d74e7d97cd055fb682919059b1283
Gennaro Avolio-Toly <gennaro.avolio-toly@smashingideas.com>
2016-10-18 15:48:41 -0700

    nineteen part 2 test working

--- nineteen.rb
+++ nineteen.rb
@@ -18,7 +18,55 @@ combos = File.readlines('nineteen.txt').map(&:strip).each do |combo|
     molecule_copy[index...(index + combo[0].size)] = combo[1]
     patterns << molecule_copy
   end
-
 end
 
-p patterns.uniq.size
+# p patterns.uniq.size
+
+@molecule = 'HOHOHO'
+part2 = 'e'
+combo2 = File.readlines('nineteen_test.txt').map(&:strip).map do |combo|
+  combo.split(' => ')
+end
+
+patterns2 = [part2]
+steps = 0
+
+until patterns2.include? @molecule
+  patterns2 = patterns2.map do |pattern|
+    new_outputs = []
+    combo2.each do |combo|
+      next unless pattern =~ /#{combo[0]}/
+
+      indexes = []
+      pattern.scan /#{combo[0]}/ do |i|
+        indexes << $~.offset(0)[0]
+      end
+
+      indexes = indexes.map do |index|
+        dup = pattern.dup
+        dup[index...(index + combo[0].size)] = combo[1]
+        dup
+      end
+      new_outputs << indexes
+    end
+    new_outputs
+  end.flatten.uniq
+
+  steps += 1
+end
+p steps
+
+  # next unless @molecule =~ /#{combo[0]}/
+  # indexes = []
+  #
+  # @molecule.scan /#{combo[0]}/ do |i|
+  #   indexes << $~.offset(0)[0]
+  # end
+  # total_indexes += indexes.size
+  #
+  # indexes.map do |index|
+  #   molecule_copy = @molecule
+  #   molecule_copy[index...(index + combo[0].size)] = combo[1]
+  #   patterns << molecule_copy
+  # end
+# end
